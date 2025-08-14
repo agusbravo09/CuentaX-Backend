6d11bac89bb68dfe6051fc65eb4a3f3bbf6ce3c9
agusbravo09 <agus0903@outlook.com>
2025-08-14 11:14:59 -0300

    First Commit

---
+++ src/test/java/com/example/FinanzApp/FinanzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinanzApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
